7b4454045a68a8c064564c69c52f811e3e890ada
unknown <iftekherakash6@gmail.com>
2025-12-07 01:32:53 +0600

    startup my project tailwind+Daisyui setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 class="text-3xl font-bold underline">RESPONSE TEAM</h1>
+      <button class="btn w-64 rounded-full">Button</button>
+      
+      
+    </>
+  )
+}
+
+export default App
